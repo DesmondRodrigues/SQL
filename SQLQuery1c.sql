229129efe2b5f9ea9266b4e64b09d5e6a1e99d86
DesmondRodrigues <desmondrodrigues@gmail.com>
2020-05-20 22:38:35 +1200

    Update SQLQuery1c.sql

--- SQLQuery1c.sql
+++ SQLQuery1c.sql
@@ -1,18 +1,18 @@
-SELECT t1.PropertyId, t2.Name AS PropertyName, t3.Amount AS RentAmount, t4.Name As FrequencyName,
+SELECT t1.PropertyId, t2.Name AS PropertyName, t3.PaymentAmount AS RentAmount, t4.Name As FrequencyName,
 		t5.StartDate As TenantStartDate, t5.EndDate As TenantEndDate, t6.Yield,
 CASE
- 	WHEN PaymentFrequencyId = 1 THEN DATEDIFF(week, StartDate, EndDate)*PaymentAmount
- 	WHEN PaymentFrequencyId = 2 THEN (DATEDIFF(week, StartDate, EndDate))/2*PaymentAmount
- 	ELSE DATEDIFF(month, StartDate, EndDate)*PaymentAmount
- 	END AS TotalRentAmount
-
+ 	WHEN t3.PaymentFrequencyId = 1 THEN t3.PaymentAmount*52
+ 	WHEN t3.PaymentFrequencyId= 2 THEN t3.PaymentAmount*26
+	WHEN t3.PaymentFrequencyId= 3 THEN t3.PaymentAmount*12
+ 	ELSE null END as TotalRentAmount
+	
 FROM OwnerProperty As t1
 	INNER JOIN Property AS t2
 	ON t1.PropertyId=t2.Id
-	INNER JOIN PropertyRentalPayment AS t3
+	INNER JOIN TenantProperty AS t3
 	ON t1.PropertyId=t3.PropertyId
-	INNER JOIN TenantPaymentFrequencies AS t4
-	ON t3.FrequencyType=t4.Id
+	INNER JOIN TargetRentType AS t4
+	ON t3.PaymentFrequencyId=t4.Id
 	INNER JOIN TenantProperty AS t5
 	ON t3.PropertyId=t5.PropertyId
 	INNER JOIN PropertyFinance AS t6
